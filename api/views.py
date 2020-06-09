8ff2d0d9e28846637a52af8df1b3f3ee91384bcf
Abdelrhman Elmahdy <AbdlrhmanElmhdy@gmail.com>
2020-06-09 21:43:51 +0200

    Used generics classes to implement the views

--- api/views.py
+++ api/views.py
@@ -1,53 +1,13 @@
-from rest_framework import status
-from django.http import Http404
-from rest_framework.views import APIView
-from rest_framework.response import Response
 from .models import User
 from .serializers import UserSerializer
+from rest_framework import generics
 
 # Create your views here.
 
-class UsersList(APIView):
-	"""
-	List all code Users, or create a new User.
-	"""
-	def get(self, request, format=None):
-		users = User.objects.all()
-		serializer = UserSerializer(users, many=True)
-		return Response(serializer.data)
+class UsersList(generics.ListCreateAPIView):
+	queryset = User.objects.all()
+	serializer_class = UserSerializer
 
-	def post(self, request, format=None):
-		data = JSONParser().parse(request)
-		serializer = UserSerializer(data=data)
-		if serializer.is_valid():
-			serializer.save()
-			return Response(serializer.data, status=status.HTTP_201_CREATED)
-		return JsonResponse(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-class UserDetail(APIView):
-	"""
-	Retrieve, update or delete a user instance.
-	"""
-	def get_object(self, pk):
-		try:
-			return User.objects.get(pk=pk)
-		except User.DoesNotExist:
-			raise Http404
-
-	def get(self, request, pk, format=None):
-		user = self.get_object(pk)
-		serializer = UserSerializer(user)
-		return Response(serializer.data)
-
-	def put(self, request, pk, format=None):
-		user = self.get_object(pk)
-		serializer = UserSerializer(user, data=request.data)
-		if serializer.is_valid():
-			serializer.save()
-			return Response(serializer.data)
-		return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-	def delete(self, request, pk, format=None):
-		user = self.get_object(pk)
-		user.delete()
-		return Response(status=status.HTTP_204_NO_CONTENT)
+class UserDetail(generics.RetrieveUpdateDestroyAPIView):
+	queryset = User.objects.all()
+	serializer_class = UserSerializer
